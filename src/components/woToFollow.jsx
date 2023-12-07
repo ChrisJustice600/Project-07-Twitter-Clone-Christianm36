e7530120b53c251ec4c4df849dcf194463015238
chris <christianmabealdi@gmail.com>
2023-12-07 17:58:45 +0100

    create component wotofollow

--- src/components/woToFollow.jsx
+++ src/components/woToFollow.jsx
@@ -1,15 +1,43 @@
+import Verified from "../images/Verified.png";
+
 export default function WoToFollow() {
+  const followProfile = [
+    {
+      avatarProfile: "src/images/Profile-Photo-new-york.png",
+      name: "The New York Times",
+      tag: "@nytimes",
+    },
+    {
+      avatarProfile: "src/images/tweet-profile-photo.png",
+      name: "CNN",
+      tag: "@CNN",
+    },
+    {
+      avatarProfile: "src/images/image 1.png",
+      name: "Twitter",
+      tag: "@Twitter",
+    },
+  ];
+
   return (
     <div className="follow">
       <h2>Who to follow</h2>
-      <div className="follow-profile">
-        <img src="" />
-        <div className="name-accompt-follow">
-          <div >
-            <div className="name">The New York Times</div>
-            <img src="" />
+      <div>
+        {followProfile.map(({ avatarProfile, name, tag }) => (
+          <div className="follow-profile">
+            <div className="left">
+              <img src={avatarProfile} />
+              <div className="name-accompt-follow">
+                <div className="title">
+                  <div className="name">{name}</div>
+                  <img src={Verified} />
+                </div>
+                <div>{tag}</div>
+              </div>
+            </div>
+            <button>Follow</button>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
