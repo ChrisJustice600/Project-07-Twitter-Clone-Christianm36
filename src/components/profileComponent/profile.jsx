85140b2bc5b8592f9a5724b57944aca3ad2db9a1
chris <christianmabealdi@gmail.com>
2023-12-10 06:08:48 +0100

    create profile details and description

--- src/components/profileComponent/profile.jsx
+++ src/components/profileComponent/profile.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReturnIcon from "../../iconprofile/return.svg"
-import MonProfile from  "../../iconprofile/mon-profile.jpg"
+import React from "react";
+import ReturnIcon from "../../iconprofile/return.svg";
+import MonProfile from "../../iconprofile/mon-profile.jpg";
+import { IoCalendarOutline } from "react-icons/io5";
+
 export default function Profile() {
   return (
     <div>
       <div className="author-profile">
         <img src={ReturnIcon} />
-        <div className='author-name'>
+        <div className="author-name">
           <div className="name">Christian mabeladi</div>
           <div className="author-post-number">0 Post</div>
         </div>
@@ -21,6 +23,33 @@ export default function Profile() {
         <div></div>
         <span>Editer profile</span>
       </div>
+      <div className="profile-informations">
+        <div className="info-name">
+          <div className="name">Christian mabeladi</div>
+          <div className="tag">@JusticeChrisM</div>
+        </div>
+        <div className="date-joined">
+          <span>
+            <IoCalendarOutline />
+          </span>
+          <div className="date">Joined December 2023</div>
+        </div>
+        <div className="section-follow-profile">
+          <div className="following">
+            <span>5</span>Following
+          </div>
+          <div className="follower">
+          <span>2</span>Follower
+          </div>
+        </div>
+        <div className="choice-section-profile">
+          <span>Posts</span>
+          <span>Peplies</span>
+          <span>Highlights</span>
+          <span>Media</span>
+          <span>Likes</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
