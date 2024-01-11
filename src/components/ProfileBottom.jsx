e7bc5c6d65fe998c513235ddaf04fbc91104b033
chris <christianmabeladi@gmail.com>
2024-01-11 11:37:14 +0100

    fix all profilebottom

--- src/components/ProfileBottom.jsx
+++ src/components/ProfileBottom.jsx
@@ -5,19 +5,18 @@ import TrendMore from "../iconSide/trend-more.svg";
 
 export default function ProfileBottom() {
     return (
-        <div>
+        <div className='down'>
             <div className="follow-profile">
                 <div className="left">
                     <img src={MonProfile} />
                     <div className="name-accompt-follow">
                         <div className="title">
                             <div className="name">Christian Mabeladi</div>
-                            <img src={TrendMore} />
-
                         </div>
                         <div>@JusticeChrisM</div>
                     </div>
                 </div>
+                <img className='more' src={TrendMore} />
             </div>
         </div>
     )
