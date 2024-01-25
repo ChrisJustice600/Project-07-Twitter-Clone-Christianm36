6bb765d9cf08a85f40ade9d109dc1510480d303f
unknown <christianmabeladi@gmail.com>
2024-01-25 13:58:00 +0100

    filteritem

--- src/components/Tweet-editor-form.jsx
+++ src/components/Tweet-editor-form.jsx
@@ -4,7 +4,8 @@ import Image3 from "../images/Poll.svg";
 import Image4 from "../images/Group.svg";
 import Image5 from "../images/Schedule (1).svg";
 import { useState } from "react";
-
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
 
 const Image = ({ src }) => {
     return (
@@ -16,34 +17,15 @@ const Image = ({ src }) => {
 
 export default function TweetEditorForm() {
     const [text, setText] = useState('');
+    const { data, setData } = useContext(UserContext);
+
+    // Cloner le tableau des tweets du contexte
+    const updatedData = [...data.tweet];
 
     const formatText = (event) => {
         const originalText = event.target.value;
-
-        //       let addObjet = [{
-        //         id: "0",
-        //         imageavatar: "",
-        //         authorName: "",
-        //         authortag: "",
-        //         date: "7min",
-        //         content: null,
-        //         imagebg: "",
-        //         reply: "19",
-        //         repost: "144",
-        //         like: "184",
-        //         bio: "It’s our job to #GoThere & tell the most difficult stories. For breaking news, follow @CNNBRK and download our app http://cnn.com/apps",
-        //         islike: false
-        //       }]
-        //     //   console.log(CopyAddObjet[0]);
-        //     // let tab = []
-        //     let modif = addObjet[0].content
-        //     modif = originalText
-
-
-
-        // Diviser le texte en lignes de 20 caractères
         const lines = originalText.match(/.{1,40}/g);
-        // Mettre à jour le texte dans l'état local avec le texte formaté
+
         if (lines) {
             setText(lines.join('\n'));
         } else {
@@ -51,6 +33,38 @@ export default function TweetEditorForm() {
         }
     };
 
+console.log(data.currentUser.userName);
+
+    const content = text.trim();
+    let addObjet = {
+        id: "0",
+        imageavatar: "",
+        userName: data.currentUser.userName,
+        authorName: "",
+        authortag: "",
+        date: "7min",
+        content: content,
+        imagebg: "",
+        reply: "19",
+        repost: "144",
+        like: "184",
+        bio: "It’s our job to #GoThere & tell the most difficult stories. For breaking news, follow @CNNBRK and download our app http://cnn.com/apps",
+        islike: false
+    };
+
+    const handleTweetClick = () => {
+        // Récupérer le contenu du texte
+      
+
+        if (content) {
+            console.log(content);
+
+            updatedData.unshift(addObjet);
+
+            setData({ ...data, tweet: updatedData });
+            setText('');
+        }
+    };
 
 
     return (
@@ -71,7 +85,7 @@ export default function TweetEditorForm() {
                     <Image src={Image4} />
                     <Image src={Image5} />
                 </div>
-                <div class="capitalize px-4 py-2 border-none rounded-full bg-blue-500 text-white text-1rem">
+                <div onClick={handleTweetClick} class="capitalize cursor-pointer px-4 py-2 border-none rounded-full bg-blue-500 text-white text-1rem">
                     <span>Tweet</span>
                 </div>
             </div>
---
+++ src/context/FilterItemContext.js
@@ -0,0 +1,3 @@
+import React from 'react'
+const FilterItemContext = React.createContext()
+export default FilterItemContext
